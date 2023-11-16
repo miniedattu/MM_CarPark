1d31310cc6e0b9227adf172bf2ec37ceeb59bb92
Mini Mathew <miniedattu@gmail.com>
2023-11-16 16:18:22 +0800

    Added constructors and attributes to the carpark class

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MM_CarPark
 ![Initial commit](Images/initial-commit.png)
+![Initial commit](Images/stubs-for-classes.png)
--- src/carpark.py
+++ src/carpark.py
@@ -1,2 +1,15 @@
 class Carpark:
-    pass
+    def __init__(self,
+                 location="Unknown",
+                 capacity=0,
+                 current_vahicle_count=0,
+                 sensors=None,
+                 displays=None):
+        self.capacity = capacity
+        self.current_vehicle_count = current_vahicle_count
+        self.location = location
+        self.displays = displays or []
+        self.sensors = sensors or []
+    def __str__(self):
+        return f"Carpark at {self.location} with a capacity of {self.capacity} vehicles"
+
