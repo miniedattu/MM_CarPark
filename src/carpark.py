5fe153b9e7dca7b4157f660e83a3e3d08f5c0829
Mini Mathew <miniedattu@gmail.com>
2023-11-23 14:57:11 +0800

    Add constructors and attributes for the class Carpark

--- src/carpark.py
+++ src/carpark.py
@@ -1,13 +1,13 @@
 class Carpark:
     def __init__(self,
-                 location="Unknown",
-                 capacity=0,
-                 current_vahicle_count=0,
+                 location,
+                 capacity,
+                 plates=None,
                  sensors=None,
                  displays=None):
-        self.capacity = capacity
-        self.current_vehicle_count = current_vahicle_count
         self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
         self.displays = displays or []
         self.sensors = sensors or []
     def __str__(self):
