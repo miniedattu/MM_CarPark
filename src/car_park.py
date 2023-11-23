8ec9f18d65c84929ba944fecdd46c43f92771c5a
Mini Mathew <miniedattu@gmail.com>
2023-11-23 16:08:55 +0800

    Added register and other methods

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,5 @@
+from sensor import Sensor
+from display import Display
 class CarPark:
     def __init__(self,
                  location,
@@ -13,3 +15,23 @@ class CarPark:
     def __str__(self):
         return f"Carpark at {self.location} with a capacity of {self.capacity} vehicles"
 
+    def register(self, component):
+        """Registers component of a car park"""
+        if not isinstance(component,(Sensor,Display)):
+            raise TypeError("Invalid component type!")
+
+        if isinstance(component,Sensor):
+            self.sensors.append(component)
+        elif isinstance(component,Display):
+            self.sensors.append(component)
+
+    def add_car(self,plate):
+        self.plates.append(plate)
+
+    def remove_car(self,plate):
+        self.plates.remove(plate)
+
+    def update_displays(self):
+        for display in self.display:
+            display.update()
+            print(f"Updating: {display}")
